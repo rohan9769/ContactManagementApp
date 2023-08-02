71588de1071fb9178b4b5f61cd042cd43469c2a2
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-01 21:32:57 -0700

    added a basic GET api

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
+app.get('/api/contacts',(req,res)=>{
+    res.send('Get All Contacts')
+})
+
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
