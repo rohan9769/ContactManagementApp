57f8842aa7ed9985e12addb25def326af363f689
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-02 21:43:28 -0700

    setup and connected MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const errorHandler = require('./middleware/errorhandler')
+const connectDb = require('./config/dbConnection')
 const dotenv  = require('dotenv').config()
 const app = express()
-
+connectDb()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
