e8dcccc3a817afce4b7657d8db3cdc78923f7a31
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-03 13:48:25 -0700

    created login and current user rout in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,25 @@
+
+
+const asyncHandler = require("express-async-handler");
 // @desc Register User
 //@route POST /api/users/register
 //@access public
-
-const asyncHandler = require("express-async-handler");
-
 const registerUser = asyncHandler(async(req,res)=>{
     res.json({message:'Register a User'})
 })
 
-module.exports = {registerUser}
+//@desc Login User
+//@route POST /api/users/login
+//@access public
+const loginUser = asyncHandler(async(req,res)=>{
+    res.json({message:'Login User'})
+})
+
+//@desc Login User
+//@route POST /api/users/login
+//@access public
+const currentUser = asyncHandler(async(req,res)=>{
+    res.json({message:'Current User'})
+})
+
+module.exports = {registerUser,loginUser,currentUser}
