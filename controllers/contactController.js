14ba9656a109c5f48211dcae3a1152b2461a7cc9
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-02 22:25:28 -0700

    created CRUD for adding a new contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -21,7 +21,12 @@ const createContact = asyncHandler(async (req,res) =>{
         res.status(400)
         throw new Error("All fields are mandatory")
     }
-    res.status(201).json({message:`Create new contact`})
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    })
+    res.status(201).json(contact)
 })
 
 //@desc Get a particular contact by ID
