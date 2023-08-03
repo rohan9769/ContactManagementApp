13220acc9d1c8642b08e1fa33aa63a40f83eb41b
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-02 22:56:49 -0700

    created CRUD for updating contact by ID

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -45,7 +45,20 @@ const getContact = asyncHandler(async (req,res) =>{
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req,res)=>{
-    res.status(200).json({message:`Update contacts for ${req.params.id}`})
+    //Fetching the contact first
+    const contact = await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404)
+        throw new Error("Contact not found")
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    )
+
+    res.status(200).json(updatedContact)
 })
 
 //@desc Update a particular contact
