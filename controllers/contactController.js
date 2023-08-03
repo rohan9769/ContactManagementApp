bd3a37d1832ed7714d72f2bfa36b970434790add
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-03 16:56:46 -0700

    final commit , done with protected routes for logged in user to update and delete contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -25,7 +25,7 @@ const createContact = asyncHandler(async (req,res) =>{
         name,
         email,
         phone,
-        // user_id:req.user.id
+        user_id:req.user.id
     })
     res.status(201).json(contact)
 })
@@ -53,6 +53,12 @@ const updateContact = asyncHandler(async (req,res)=>{
         throw new Error("Contact not found")
     }
 
+    //Checking if fetched contact has id
+    if(contact.user_id.toString()!==req.user.id){
+        res.status(403)
+        throw new Error("User don't have permission to update other user contact")
+    }
+
     const updatedContact = await Contact.findByIdAndUpdate(
         req.params.id,
         req.body,
@@ -72,7 +78,11 @@ const deleteContact = asyncHandler(async (req,res)=>{
         res.status(404)
         throw new Error("Contact not found")
     }
-    await Contact.deleteOne();
+    if(contact.user_id.toString()!==req.user.id){
+        res.status(403)
+        throw new Error("User don't have permission to delete other user contact")
+    }
+    await Contact.deleteOne({_id:req.params.id});
     res.status(200).json(contact)
 })
 module.exports = {getContacts,createContact,getContact,updateContact,deleteContact}
