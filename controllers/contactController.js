5e72bfbbd59da82b7a2fd1eb1412bbdc962c0e8d
Rohan Nayak <rohannayak2071@gmail.com>
2023-08-03 16:39:42 -0700

    making all routes for contact protected

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,7 @@ const Contact = require('../models/contactModel')
 
 //@desc Get all contacts
 //@route GET /api/contacts
-//@access public
+//@access private
 const getContacts = asyncHandler(async(req,res) =>{
     const contacts = await Contact.find()
     res.status(200).json(contacts)
@@ -13,7 +13,7 @@ const getContacts = asyncHandler(async(req,res) =>{
 
 //@desc Create new contact
 //@route POST /api/contacts
-//@access public
+//@access private
 const createContact = asyncHandler(async (req,res) =>{
     console.log(req.body)
     const {name,email,phone} = req.body
@@ -31,7 +31,7 @@ const createContact = asyncHandler(async (req,res) =>{
 
 //@desc Get a particular contact by ID
 //@route GET /api/contacts/:id
-//@access public
+//@access private
 const getContact = asyncHandler(async (req,res) =>{
     const contact = await Contact.findById(req.params.id)
     if(!contact){
